d443d5c9be19cfee78ee9dcfdb3b6032104c793e
sonhyein <shl6900@naver.com>
2020-10-19 12:55:01 +0900

    불필요한 패키지 삭제

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { persistStore } from "redux-persist";
-import { PersistGate } from "redux-persist/integration/react";
 import configureStore from "store/configureStore";
 import Feed from "./Feed";
 import GlobalStyle from "styles/GlobalStyle";
 
 const store = configureStore();
-const persistor = persistStore(store);
 
 ReactDOM.render(
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor} />
     <GlobalStyle />
     <Feed />
   </Provider>,
--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,14 +1,6 @@
 import { combineReducers } from "redux";
-import { persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
 import CardReducer from "./card";
 import { CardState } from "store/types";
-
-const persistConfig = {
-  key: "root",
-  storage,
-  blacklist: ["CardReducer"],
-};
 
 export interface RootState {
   CardReducer: CardState;
@@ -18,4 +10,4 @@ const rootReducer = combineReducers<RootState>({
   CardReducer,
 });
 
-export default persistReducer(persistConfig, rootReducer);
+export default rootReducer;
